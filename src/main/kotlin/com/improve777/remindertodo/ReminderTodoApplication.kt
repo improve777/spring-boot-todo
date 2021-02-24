d8b50f723596c0244c91c9567c7634a2f93c7b21
improve777 <improve777@gmail.com>
2021-02-25 00:44:53 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/improve777/remindertodo/ReminderTodoApplication.kt
@@ -0,0 +1,11 @@
+package com.improve777.remindertodo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReminderTodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReminderTodoApplication>(*args)
+}
